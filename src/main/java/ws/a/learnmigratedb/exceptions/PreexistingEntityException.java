7bd4d99f878fcd2e31f73c08962d0f49f917a617
Reyfadh07 <raihan.farmasi@gmail.com>
2022-10-18 12:56:21 +0700

    menambahkan jpa controller

---
+++ src/main/java/ws/a/learnmigratedb/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package ws.a.learnmigratedb.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
