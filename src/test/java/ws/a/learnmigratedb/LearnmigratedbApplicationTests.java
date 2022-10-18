b7d6bd04405f9e67b340c84e041710984699be09
Reyfadh07 <raihan.farmasi@gmail.com>
2022-10-18 12:36:36 +0700

    firstcommit

---
+++ src/test/java/ws/a/learnmigratedb/LearnmigratedbApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.learnmigratedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnmigratedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
